344b16a08b039ca0904ef0f493aeb9a57dea57dc
sohag-arnifi <sohag.b@arnifi.com>
2024-03-15 05:21:00 +0600

    generate template api code route change

--- src/app/api/templates/[id]/route.ts
+++ src/app/api/templates/[id]/route.ts
@@ -4,6 +4,11 @@ import httpStatus from "http-status";
 import sendResponse from "@/utils/server/helpers/sendResponse";
 import { TemplateService } from "../templates.service";
 import { ITemplate } from "../templates.model";
+import { LegalDocumentService } from "../../legal-documents/legalDocument.service";
+import generateTemplate from "@/utils/client/generateTemplate";
+import Legal_Documents, {
+  ILegalDocument,
+} from "../../legal-documents/legalDocument.model";
 
 export const GET = catchAsync(
   async (req: Request, res: Response): Promise<NextResponse> => {
@@ -31,6 +36,32 @@ export const PATCH = catchAsync(
       success: true,
       message: "Template Update Successfully",
       data: response,
+    });
+  }
+);
+
+export const POST = catchAsync(
+  async (req: Request, res: Response): Promise<NextResponse> => {
+    const id = req.url.split("/").pop();
+    const data = await req.json();
+
+    const document = await (
+      await LegalDocumentService.findOne(id as string)
+    ).toJSON();
+    const htmlTemp = generateTemplate(document as ILegalDocument, data);
+
+    await Legal_Documents.update(
+      { id },
+      {
+        downloadCount: document?.downloadCount + 1,
+      }
+    );
+
+    return await sendResponse({
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Document Template Generated Successfully",
+      data: htmlTemp,
     });
   }
 );
